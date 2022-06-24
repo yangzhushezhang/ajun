c11147a71c53d123de4aac928841d48b63b192f5
unknown <1508612234@qq.com>
2022-06-24 22:37:02 +0800

    2022年6月24日22:37:01

--- main.go
+++ main.go
@@ -13,10 +13,20 @@ func main() {
 	if err != nil {
 		fmt.Println("错误:", err.Error())
 	}
-
+	b := string("information")
+	c := "李狗蛋"
+	d := "男"
+	e := "35"
 	a := make([]model.Information, 0)
-	db.Raw("INSERT INTO information (name,sex,age) VALUES('肖','男',30)").Scan(&a)
-	db.Raw("SELECT * FROM information").Scan(&a)
-	fmt.Println(a)
-
+	db.Raw("TRUNCATE TABLE " + b).Scan(&a)                                                               //清空表格所有信息
+	db.Raw("SELECT *FROM information").Scan(&a)                                                          //查询表格所有信息
+	db.Raw("INSERT INTO information(name,sex,age) VALUES  ('" + c + "','" + d + "'," + e + ")").Scan(&a) //增
+	db.Raw("DELETE FROM information WHERE name='李'")                                                     //删
+	db.Raw("UPDATE information SET ID='1',`name`='杨',sex='女',age=25 WHERE  `name`='肖'").Scan(&a)         //改
+	db.Raw("select * from information where `name`='李'")                                                 //查
+	//http.HandleFunc("/", model.Post)
+	//err = http.ListenAndServe(":6666", nil)
+	//if err != nil {
+	//	fmt.Println("err:", err)
+	//}
 }
